fd4cccd593685e5ad520b41409aa71d981b670e0
João Lucas Santos <joao__lucas_santos@hotmail.com>
2016-01-13 21:56:58 -0200

    Classe Usuario

--- AlmoxarifadoLalu/src/classesBasicas/Usuario.java
+++ AlmoxarifadoLalu/src/classesBasicas/Usuario.java
@@ -1,5 +1,46 @@
 package classesBasicas;
 
 public class Usuario {
+	
+	public String nome;
+	public String CPF;
+	public String login;
+	public String senha;
+	
+	public Usuario(String nome, String cPF, String login, String senha) {
+		this.nome = nome;
+		CPF = cPF;
+		this.login = login;
+		this.senha = senha;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	
+	
 }
